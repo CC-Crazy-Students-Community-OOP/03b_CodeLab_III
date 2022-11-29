1cb6e033c1e63ecce85a192a7407147590db1704
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-29 14:45:17 +0100

    test

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,15 +2,9 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
-     
+    public static void main( String[] args ) {
+        
     }
-
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
-    }
-
-
-
+    public static void output( String o ) { System.out.println( o ); }
 }
 
